4b49a18a724015cf10e352c4b2bb4b8ad17fbb5d
Peter Malinowski <petermalinowski26@gmail.com>
2021-07-07 12:25:50 +0300

    FirstApp added

---
+++ FirstApp/src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("First line from App");
+        System.out.println("Second line from App");
+        System.out.println("Third line from App");
+
+    }
+}
